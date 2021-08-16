97aae32c7a2aef337ab7c0a9148360061b4d29c2
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-16 23:58:14 +0900

    update models/user

--- models/user.go
+++ models/user.go
@@ -8,14 +8,23 @@ import (
 
 type User struct {
 	gorm.Model
-	Name     string    `gorm:"not null"`
-	Email    string    `gorm:"unique;not null"`
-	Password string    `gorm:"not null"`
-	Pictures []Picture `gorm:"foreignKey:UserId"`
-	Likes    []Like    `gorm:"foreignKey:UserId"`
-	Comments []Comment `gorm:"foreignKey:UserId"`
+	Name     string    `gorm:"not null" binding:"required,max=24,min=1"`
+	Email    string    `gorm:"unique;not null" binding:"required,email,max=100"`
+	Password []byte    `gorm:"not null"`
+	Pictures []Picture `gorm:"foreignKey:UserId" binding:"-"`
+	Likes    []Like    `gorm:"foreignKey:UserId" binding:"-"`
+	Comments []Comment `gorm:"foreignKey:UserId" binding:"-"`
 }
 
 func init() {
 	db.AutoMigrateDB(&User{})
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	return db.Validate.Struct(u)
+}
+
+func (u *User) Create() error {
+	db := db.GetDB()
+	return db.Create(u).Error
 }
