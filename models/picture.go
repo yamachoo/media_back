b834e63d696dfe486b114074b83dccb52fb6b46e
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-08-23 21:49:17 +0900

    update models/picture

--- models/picture.go
+++ models/picture.go
@@ -13,13 +13,22 @@ type Picture struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
-	UserId    uint           `gorm:"not null"`
-	Filename  string         `gorm:"not null"`
-	Picture   []byte         `gorm:"not null"`
-	Likes     []Like         `gorm:"foreignKey:PictureId"`
-	Comments  []Comment      `gorm:"foreignKey:PictureId"`
+	UserId    uint           `gorm:"not null" binding:"required"`
+	Filename  string         `gorm:"not null" binding:"required,max=100,min=1"`
+	Path      string         `gorm:"not null" binding:"required"`
+	Likes     []Like         `gorm:"foreignKey:PictureId" binding:"-"`
+	Comments  []Comment      `gorm:"foreignKey:PictureId" binding:"-"`
 }
 
 func init() {
 	db.AutoMigrateDB(&Picture{})
+}
+
+func (p *Picture) BeforeSave(tx *gorm.DB) error {
+	return db.Validate.Struct(p)
+}
+
+func (p *Picture) Create() error {
+	db := db.GetDB()
+	return db.Create(p).Error
 }
